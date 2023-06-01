9d5b2db39ac1b6b83a3f4c9e42c1beb2fedbcb6f
kyunghee kwak <kwakkyunghee325@gmail.com>
2023-06-01 09:02:56 +0900

    강아지, 고양이 개별 클래스 구현 ///주석

---
+++ InheritancePrac/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InheritancePrac
+{
+    /// <summary>
+    /// 고양이 클래스
+    /// </summary>
+    public class Cat
+    {
+        /// <summary>
+        /// 고양이 나이
+        /// </summary>
+        /// <value>나이</value>
+        public int Age { get; set; }
+
+        public void Meow()
+        {
+            Console.WriteLine("냥냥 웁니다");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("냠냠 먹습니다.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("쿨쿨 잠을 잡니다.");
+        }
+    }
+}
--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -10,6 +10,22 @@ namespace InheritancePrac
     {
         static void Main(string[] args)
         {
+            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+            //Dogs처럼 객체가 여러개가 있다는 것을 알려주는 게 좋다
+
+            foreach ( var item in Dogs )
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+            foreach ( var item in Cats )
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
         }
     }
 }
